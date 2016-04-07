be53c05e465f9d3d9d0bd37a1e9e60713c2d2f0c
Clement DUNGLER <clement.dungler@solitonit.com>
2016-04-07 17:30:05 +0200

    Add translation code and files

---
+++ langs/lang.fr.php
@@ -0,0 +1,111 @@
+<?php
+/*
+------------------
+Language: French
+------------------
+*/
+
+$lang = array();
+
+$lang['EN_ACTIVE'] = '';
+$lang['FR_ACTIVE'] = 'active';
+// Menu
+
+$lang['MENU_EDUCATION'] = 'Éducation';
+$lang['MENU_EXPERIENCE'] = 'Expérience';
+$lang['MENU_EXPERTISE'] = 'Expertise technique';
+$lang['MENU_HOBBIES'] = 'Loisirs';
+$lang['MENU_DOWNLOAD'] = 'Télécharger CV';
+
+//Hero
+$lang['HERO_TITLE'] = 'Développeur Logiciel &amp; Front End basé à Paris';
+$lang['HERO_SUB'] = 'Bienvenue sur mon CV-site';
+$lang['HERO_DOWNLOAD'] = 'Télécharger CV';
+$lang['HERO_CONTACT'] = 'Contact';
+
+//About me
+$lang['ABOUT_TITLE'] = 'DÉVELOPPEUR LOGICIEL &amp; FRONT-END PARIS';
+$lang['ABOUT_P1'] = 'Hey, Je suis Clement Dungler! Un développeur logiciel et front-end enthousiaste et motivé en provenance de Paris. Je suis passionné par la création d\'expériences utilisateurs agréables en utilisant des outils novateurs. La communication et une attitude positive sont pour moi, des valeurs clés pour la réussite d\'un projet.';
+$lang['ABOUT_P2'] = 'Actuellement travaillant chez <a href="http://www.solitonit.com/" class="domain">Soliton IT</a> en tant que développeur logiciel, Je met en pratique mes connaissances, en créant des sites internet, logiciel et applications mobile';
+$lang['ABOUT_S1_TITLE'] = 'Logiciel';
+$lang['ABOUT_S1_CONTENT'] = 'Je suis passioné par la creation de logiciel agréable à utiliser avec une architecture et un design réflechit en utilisant la puissance des languages orientés objects.';
+$lang['ABOUT_S2_TITLE'] = 'Front-end';
+$lang['ABOUT_S2_CONTENT'] = 'J\'adore créer des interfaces utilisateur offrant une experience agréable par l\'utilisation des dernières technologies en se concentrant sur HTML5, CSS3, JS.';
+$lang['ABOUT_S3_TITLE'] = 'Design adapté';
+$lang['ABOUT_S3_CONTENT'] = 'Pour assurer la meilleure expérience utilisateur possible, les sites doivent fonctionner indépendamment de la plateforme utilisée à un moment donné.';
+
+//Education
+$lang['EDUCATION_TITLE'] = 'Éducation';
+$lang['EDUCATION_E1_TITLE'] = 'École d\'ingénieur – ENSISA';
+$lang['EDUCATION_E1_DATES'] = '2010-2013 @ Mulhouse, France';
+$lang['EDUCATION_E1_DESC'] = 'École d\'ingénieur spécialisée en informatique et réseaux';
+$lang['EDUCATION_E2_TITLE'] = 'Classe préparatoire aux grandes écoles';
+$lang['EDUCATION_E2_DATES'] = '2008-2010 @ Mulhouse, France';
+$lang['EDUCATION_E3_TITLE'] = 'Baccalauréat avec mention';
+$lang['EDUCATION_E3_DATES'] = '2008 @ Mulhouse, France';
+
+//Experience
+$lang['EXPERIENCE_TITLE'] = 'Expérience';
+$lang['EXPERIENCE_E1_COMPANY'] = 'Soliton IT Ltd';
+$lang['EXPERIENCE_E1_DATES'] = 'Oct 2013 - Maintenant @ Hemel Hempstead (UK) / Bureau à domicile (France)';
+$lang['EXPERIENCE_E1_TITLE'] = 'Développeur logiciel';
+$lang['EXPERIENCE_E1_DESC1'] = 'Développement front-end et back-end';
+$lang['EXPERIENCE_E1_DESC1_1'] = 'Création et déploiement du site de support de l\'entreprise. La partie front-end utilise les concepts de "material design". La partie back-end est une Web API Microsoft .NET <a href="http://178.21.237.178:8080" target="blank">lien</a>';
+$lang['EXPERIENCE_E1_DESC1_2'] = 'Remaniement du site web de l\'entreprise. Multi-langue, design adapté <a href="http://www.solitonit.com" target="blank">lien</a>';
+$lang['EXPERIENCE_E1_DESC1_3'] = 'Définition, création et déploiement en tant que web application d\'une application mobile pour different hopitaux';
+$lang['EXPERIENCE_E1_DESC2'] = 'Développement logiciel';
+$lang['EXPERIENCE_E1_DESC2_1'] = 'Définition, implémentation et test de nouveaux modules pour le systeme d\'information du departement de radiologie des hopitaux';
+$lang['EXPERIENCE_E1_DESC2_2'] = 'Résolution de bugs et améliorations générales';
+$lang['EXPERIENCE_E1_DESC2_3'] = 'Remaniement de l\'interface utilisateur';
+$lang['EXPERIENCE_E2_COMPANY'] = 'Airbus';
+$lang['EXPERIENCE_E2_DATES'] = 'Mar 2013 - Aug 2013 @ Toulouse, France';
+$lang['EXPERIENCE_E2_TITLE'] = 'Stagiaire développeur logiciel';
+$lang['EXPERIENCE_E2_DESC1'] = 'Amélioration d\'une solution, étude de l\'éxistant et de son context';
+$lang['EXPERIENCE_E2_DESC2'] = 'Faisabilité et propositions techniques, développement en ASP .NET et SQL Server, test et validation';
+$lang['EXPERIENCE_E2_DESC3'] = 'Gestion de projet et validation par les utilisateurs';
+$lang['EXPERIENCE_E3_COMPANY'] = 'AMES (Aircraft Maintenance Engineering Services)';
+$lang['EXPERIENCE_E3_DATES'] = 'Vacances 2011 - 2012 @ Saint-Louis, France';
+$lang['EXPERIENCE_E3_TITLE'] = 'Assistant ingénieur';
+$lang['EXPERIENCE_E3_DESC1'] = 'Développement d\'applications Java, modification d\'un site web sous Joomla';
+$lang['EXPERIENCE_E3_DESC2'] = 'Géneration automatique et export de documents de maintenance d\'avion et publication sur un site web';
+
+//Expertise
+$lang['EXPERTISE_TITLE'] = 'Expertise';
+$lang['EXPERTISE_LANGUAGES'] = 'Languages';
+$lang['EXPERTISE_YEARS'] = 'années';
+$lang['EXPERTISE_CS'] = 'C#';
+$lang['EXPERTISE_WPF'] = 'WPF';
+$lang['EXPERTISE_ASPNET'] = 'ASP.NET 4+, MVC';
+$lang['EXPERTISE_WEBAPI'] = 'Web API';
+$lang['EXPERTISE_HTML'] = 'HTML 5, CSS 3, JS';
+$lang['EXPERTISE_SQL'] = 'SQL';
+$lang['EXPERTISE_JAVA'] = 'Java';
+$lang['EXPERTISE_SOFTWARE'] = 'Logiciel';
+$lang['EXPERTISE_VS'] = 'Visual Studio';
+$lang['EXPERTISE_SQLSERVER'] = 'SQL Server';
+$lang['EXPERTISE_GIT'] = 'Sourcetree (git)';
+$lang['EXPERTISE_XCODE'] = 'Xcode';
+$lang['EXPERTISE_SKILLS'] = 'Compétences';
+$lang['EXPERTISE_TEAM'] = 'Travail d\'équipe';
+$lang['EXPERTISE_TEAM_DESC'] = 'Jamais seul';
+$lang['EXPERTISE_COMM'] = 'Communication';
+$lang['EXPERTISE_PROJECT'] = 'Gestion de project ';
+$lang['EXPERTISE_INDEPENDANT'] = 'Indépendant';
+$lang['EXPERTISE_INDEPENDANT_DESC'] = 'Complètement';
+
+//Hobbies
+$lang['HOBBIES_H1_TITLE'] = 'Cuisine';
+$lang['HOBBIES_H1_DESC'] = 'En tant que bon Français, bien manger est vraiment important pour moi. Même dans ma petite cuisine, cela ne me dérange pas de cuisiner des plats qui exigent une longue préparation.';
+$lang['HOBBIES_H2_TITLE'] = 'Course à pied';
+$lang['HOBBIES_H2_DESC'] = 'J\'ai commencé à courir il y a plus d\'un an. Travaillant depuis mon domicile, j\'ai eu besoin d\'une raison pour sortir chaque jour. J\'ai rapidement décidé de préparer un semi-marathong que j\'ai completé en 2 heures en octobre dernier. Je cours toujours et continu d\'essayer d\'améliorer ce temps!';
+$lang['HOBBIES_H3_TITLE'] = 'Films / Séries';
+$lang['HOBBIES_H3_DESC'] = 'Je passe un certain temps à regarder des films et séries. Je ne parviens pas à être à jour sur toutes les séries que je regarde ce qui vous laisse deviner qu\'il y en a beaucoup!';
+
+//Footer
+$lang['FOOTER_TITLE'] = 'Prendre contact';
+$lang['FOOTER_DESC'] = 'Si vous êtes à la recherche d\'un développeur logiciel ou front-end pour vos futurs projets, contactez-moi.';
+$lang['FOOTER_COPY'] = 'Copyright © 2016 ';
+$lang['FOOTER_SOCIAL'] = 'Réseau social';
+
+
+?>
