28be525561d0c1f6628880fe1a619d30cc6d3c33
Clement DUNGLER <clementdungler@gmail.com>
2019-05-20 11:50:46 +0200

    Invalidate cloudfront

--- src/helpers/i18n.ts
+++ src/helpers/i18n.ts
@@ -1,11 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import backend from "i18next-xhr-backend";
+const isProd = process.env.NODE_ENV === "production";
 i18n
   .use(backend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    debug: true,
+    debug: !isProd,
     load: "languageOnly",
     lng: "fr",
     fallbackLng: "fr",
@@ -16,6 +17,6 @@ i18n
     },
     saveMissing: true,
     missingKeyHandler: function(lng, ns, key, fallbackValue) {
-      console.warn("Missing key", lng, ns, key);
+      !isProd && console.warn("Missing key", lng, ns, key);
     }
   });
